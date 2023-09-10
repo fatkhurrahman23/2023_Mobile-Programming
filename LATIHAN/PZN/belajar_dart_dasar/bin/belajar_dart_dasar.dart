7a4e06f22217f62bdc65ea30ed30a02b2632fdb5
Moh Fatkhur Rahman <akufatkhur62@gmail.com>
2023-09-10 12:40:46 +0700

    first commit

---
+++ LATIHAN/PZN/belajar_dart_dasar/bin/belajar_dart_dasar.dart
@@ -0,0 +1,15 @@
+import 'package:belajar_dart_dasar/belajar_dart_dasar.dart' as belajar_dart_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_dasar.calculate()}!');
+  var halo = "hai";
+  var hai = 'halo';
+
+  //function to check data type
+  print(halo.runtimeType);
+
+  print(halo);
+  print(hai);
+
+}
+
