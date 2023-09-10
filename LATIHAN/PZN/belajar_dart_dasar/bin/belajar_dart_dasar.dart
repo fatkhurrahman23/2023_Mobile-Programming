e239771074091950c3697c1e0a6664f206941b7a
Moh Fatkhur Rahman <akufatkhur62@gmail.com>
2023-09-10 12:51:06 +0700

    update

--- LATIHAN/PZN/belajar_dart_dasar/bin/belajar_dart_dasar.dart
+++ LATIHAN/PZN/belajar_dart_dasar/bin/belajar_dart_dasar.dart
@@ -2,14 +2,5 @@ import 'package:belajar_dart_dasar/belajar_dart_dasar.dart' as belajar_dart_dasa
 
 void main(List<String> arguments) {
   print('Hello world: ${belajar_dart_dasar.calculate()}!');
-  var halo = "hai";
-  var hai = 'halo';
-
-  //function to check data type
-  print(halo.runtimeType);
-
-  print(halo);
-  print(hai);
-
 }
 
